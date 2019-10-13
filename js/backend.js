60ba513f73b597907fb2b94be65b923ffa398991
b-612 <fromtheorion@yandex.ru>
2019-10-13 21:00:40 +0300

    Добавляет код для получения данных с сервера. Но нужны еще доработки с выводом ошибок

---
+++ js/backend.js
@@ -0,0 +1,80 @@
+'use strict';
+
+(function () {
+  var URL_FOR_LOAD = 'https://js.dump.academy/keksobooking/data';
+  var METHOD_FOR_LOAD = 'GET';
+
+  var REQUEST_TIMEOUT = 15000;
+  var STATUS_OK = 200;
+
+  var getXhr = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    return xhr;
+  };
+
+  var getXhrParams = function (xhr, url, method, timeout, data) {
+    xhr.timeout = timeout;
+    xhr.open(method, url);
+
+    switch (data) {
+      case null :
+        xhr.send();
+        break;
+      default :
+        xhr.send(data);
+        break;
+    }
+
+    return xhr.response;
+  };
+
+  var getErrorMessage = function (xhr) {
+    var errorPrew = 'Ошибка: ';
+    var errorStatus = 'Статус ошибки: ' + xhr.status;
+    var errorMessage;
+
+    switch (xhr.status) {
+      case 400 :
+        errorMessage = 'Неверный запрос';
+        break;
+      case 401 :
+        errorMessage = 'Пользователь не авторизован';
+        break;
+      case 404 :
+        errorMessage = 'Ничего не найдено';
+        break;
+      default :
+        errorMessage = 'Что-то пошло не так';
+    }
+
+    return {
+      errorPrew: errorPrew,
+      message: errorPrew + errorMessage + '. ' + errorStatus
+    };
+  };
+
+  var setLoadCallback = function (xhr, onLoad, onError) {
+    xhr.addEventListener('load', function () {
+      if (xhr.status === STATUS_OK) {
+        var pins = onLoad(xhr.response);
+        window.pins.addPinsListeners(pins, xhr.response);
+      } else {
+        onError(getErrorMessage(xhr).message);
+      }
+    });
+  };
+
+  var getAllPinsData = function (onLoad, onError) {
+    var xhr = getXhr();
+
+    getXhrParams(xhr, URL_FOR_LOAD, METHOD_FOR_LOAD, REQUEST_TIMEOUT, null);
+    setLoadCallback(xhr, onLoad, onError);
+  };
+
+  window.backend = {
+    getAllPinsData: getAllPinsData
+  };
+})();
+
--- js/pins.js
+++ js/pins.js
@@ -6,7 +6,7 @@
     .content.querySelector('.map__pin');
   var pinsList = document.querySelector('.map__pins');
 
-  var makePins = function () {
+  var makePins = function (allPins) {
     var pins = [];
     var pinWidth = 50;
     var pinHeight = 70;
@@ -14,13 +14,13 @@
     for (var i = 0; i < window.data.addressesQuantiti; i++) {
       var pin = pinTemplate.cloneNode(true);
       var pinImage = pin.querySelector('img');
-      var pinLocationX = window.data.allBookingProps[i].location.x - pinWidth / 2;
-      var pinLocationY = window.data.allBookingProps[i].location.y - pinHeight;
+      var pinLocationX = allPins[i].location.x - pinWidth / 2;
+      var pinLocationY = allPins[i].location.y - pinHeight;
 
       pin.setAttribute('style', 'left: ' + pinLocationX +
         'px; ' + 'top: ' + pinLocationY + 'px');
-      pinImage.setAttribute('src', window.data.allBookingProps[i].author.avatar);
-      pinImage.setAttribute('alt', window.data.allBookingProps[i].offer.title);
+      pinImage.setAttribute('src', allPins[i].author.avatar);
+      pinImage.setAttribute('alt', allPins[i].offer.title);
 
       pins.push(pin);
     }
@@ -28,8 +28,8 @@
     return pins;
   };
 
-  var renderFragmentPins = function () {
-    var pins = makePins();
+  var renderFragmentPins = function (allPins) {
+    var pins = makePins(allPins);
     for (var i = 0; i < window.data.addressesQuantiti; i++) {
       window.util.fragment.appendChild(pins[i]);
     }
@@ -43,31 +43,31 @@
     map.classList.remove('map--faded');
   };
 
-  var showPins = function () {
-    var pins = renderFragmentPins();
+  var showPins = function (allPins) {
     showMap();
+    var pins = renderFragmentPins(allPins);
 
     return pins;
   };
 
-  var onPinClick = function (i) {
+  var onPinClick = function (allPins, i) {
     return function () {
-      window.card.renderFragmentPopup(window.card.makeMapPopup(window.data.allBookingProps[i]));
+      window.card.renderFragmentPopup(window.card.makeMapPopup(allPins[i]));
     };
   };
 
-  var onPinEnterPress = function (i) {
+  var onPinEnterPress = function (allPins, i) {
     return function (evt) {
       if (evt.keyCode === window.util.ENTER_KEYCODE) {
-        window.card.renderFragmentPopup(window.card.makeMapPopup(window.data.allBookingProps[i]));
+        window.card.renderFragmentPopup(window.card.makeMapPopup(allPins[i]));
       }
     };
   };
 
-  var addPinsListeners = function (mapPins) {
+  var addPinsListeners = function (mapPins, allPinsProp) {
     for (var i = 0; i < mapPins.length; i++) {
-      mapPins[i].addEventListener('click', onPinClick(i));
-      mapPins[i].addEventListener('keydown', onPinEnterPress(i));
+      mapPins[i].addEventListener('click', onPinClick(allPinsProp, i));
+      mapPins[i].addEventListener('keydown', onPinEnterPress(allPinsProp, i));
     }
   };
 
