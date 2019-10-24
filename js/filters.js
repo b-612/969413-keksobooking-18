dbc92f1dc62df593b5dac6cddfa42d8a144d9922
b-612 <fromtheorion@yandex.ru>
2019-10-24 20:10:03 +0300

    Добавляет реализацию фильтров вместе с фильтром по цене. Пока что только селекты, без чекбоксов с фичами

--- js/filters.js
+++ js/filters.js
@@ -1,6 +1,11 @@
 'use strict';
 
 (function () {
+  var Prices = {
+    LOW: 9999,
+    HIGH: 50001
+  };
+
   var offersFiltersMap = {
     '0': 'type',
     '1': 'price',
@@ -9,9 +14,9 @@
   };
 
   var filters = window.form.mapFilters;
+  var filterSelects = filters.querySelectorAll('select');
 
   var getSelectsValues = function () {
-    var filterSelects = filters.querySelectorAll('select');
     var selectedValues = [];
     filterSelects.forEach(function (current) {
       selectedValues.push(current.value);
@@ -32,41 +37,62 @@
     return passingScore;
   };
 
+  var getPriceLevel = function (currentPrice) {
+    var priceLevel;
+
+    switch (true) {
+      case currentPrice <= Prices.LOW :
+        priceLevel = 'low';
+        break;
+      case currentPrice >= Prices.HIGH :
+        priceLevel = 'high';
+        break;
+      default :
+        priceLevel = 'middle';
+    }
+
+    return priceLevel;
+  };
+
   var getRank = function (currentOffer, selectedFilters) {
     var rank = 0;
-
     selectedFilters.forEach(function (currentSelect, index) {
-      // debugger;
       var offerParam = currentOffer.offer[offersFiltersMap[index]];
 
-      if (typeof offerParam === 'number') {
+      if (typeof offerParam === 'number' && offersFiltersMap[index] !== 'price') {
         currentSelect = Number(currentSelect);
       }
 
       if (offerParam === currentSelect) {
         rank++;
       }
+
+      if (offersFiltersMap[index] === 'price') {
+        var priceLevel = getPriceLevel(currentOffer.offer.price);
+        if (priceLevel === currentSelect) {
+          rank++;
+        }
+      }
     });
 
     return rank;
   };
 
-  var getFilteredOffers = function (allOffers) {
-    // debugger;
+  var onFilterSelectChange = function () {
     var selectedValues = getSelectsValues();
     var passingScore = getPassingScore(selectedValues);
     var filteredOffers = [];
 
     switch (true) {
       case passingScore > 0 :
-        allOffers.forEach(function (currentOffer) {
+        window.backend.downloadPins.forEach(function (currentOffer) {
           if (getRank(currentOffer, selectedValues) === passingScore) {
             filteredOffers.push(currentOffer);
           }
         });
         break;
       default :
-        filteredOffers = allOffers;
+        filteredOffers = window.backend.downloadPins;
     }
 
     window.formSubmit.removePins();
@@ -74,9 +100,15 @@
     window.pins.addPinsListeners(pins, filteredOffers);
   };
 
+  var setSelectCallback = function () {
+    filterSelects.forEach(function (currentSelect) {
+      currentSelect.addEventListener('change', onFilterSelectChange);
+    });
+  };
+
+  setSelectCallback();
+
   window.filters = {
-    getSelectsValues: getSelectsValues,
-    getPassingScore: getPassingScore,
-    getFilteredOffers: getFilteredOffers
+    onFilterSelectChange: onFilterSelectChange
   };
 })();
